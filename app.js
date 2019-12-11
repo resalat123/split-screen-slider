0483c359d13764f7300fa2a47012b603722a8fd5
resalat123 <resalatamin@gmail.com>
2019-12-11 14:39:01 +0600

    simple css magic slider

---
+++ app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let wrapper = document.getElementById('wrapper');
+    let topLayer = document.querySelector('.top');
+    let hanlde = document.querySelector('.handle');
+    let skew = 0;
+    let delta = 0;
+
+    if(wrapper.className.indexOf('skewed')!= -1){
+        skew = 990;
+    }
+    wrapper.addEventListener('mousemove', function(e){
+        delta = (e.clientX - window.innerWidth/2)*0.5;
+        hanlde.style.left = e.clientX + delta + 'px';
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    });
+});
